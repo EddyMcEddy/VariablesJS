a4c86f4c2f4656d4aad110bffa71f6ac7f532e2e
Eddy Huapaya <ehuap@yahoo.com>
2023-06-21 22:32:59 -0400

    finishing doing math

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,51 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+//import typescriptLogo from "./typescript.svg";
+//import viteLogo from "/vite.svg";
+//import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+//Practice Creating a Variable of Coffees Drank
+const numberOfCupsOfCoffee = 3;
+//storing my Ful Name under the variable fullName
+const fullName = "Eduard Huapaya";
+// Printing out the full Name and amount of Coffees
+console.log(`I ${fullName} drink about ${numberOfCupsOfCoffee} coffees a day `);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+//Creating an Object with properties
+const aboutMe = {
+  fullName,
+  luckyNumber: 24,
+  favoriteMovies: [`Inception`, `The Batman: DarkNight`, `Toy-Story`],
+};
+
+//Printing out the properties from the OBJECT
+console.log(
+  `I ${aboutMe.fullName} luckiest number is ${aboutMe.luckyNumber} and my favorite movies are ${aboutMe.favoriteMovies} `
+);
+
+//Asking for their name
+const userName = window.prompt(`Hi, what is your name?: `);
+//Printing out their name
+console.log(`Hi ${userName}, Have a great day!`);
+
+//Asking for numbers turning them into strings
+console.log(`what are your favorite numbers?: `);
+
+//document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+//<div>
+//  <a href="https://vitejs.dev" target="_blank">
+//    <img src="${viteLogo}" class="logo" alt="Vite logo" />
+// </a>
+//  <a href="https://www.typescriptlang.org/" target="_blank">
+//    <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//   </a>
+//  <h1>Vite + TypeScript</h1>
+//  <div class="card">
+//    <button id="counter" type="button"></button>
+//   </div>
+//   <p class="read-the-docs">
+//    Click on the Vite and TypeScript logos to learn more
+//  </p>
+//// </div>
+//`;
+
+//setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
